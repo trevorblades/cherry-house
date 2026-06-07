06d97d742b38cc4d534f7e2e9c28a05ea808ff2a
Trevor Blades <tdblades@gmail.com>
2026-06-07 12:49:03 -0700

    add branding and content

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,5 @@
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import typesafeRoutes from "astro-typesafe-routes";
 
@@ -8,4 +8,25 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "CodecPro",
+      cssVariable: "--font-codec-pro",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/CodecPro-Regular.woff2"],
+            weight: 400,
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/CodecPro-Bold.woff2"],
+            weight: 700,
+            style: "normal",
+          },
+        ],
+      },
+    },
+  ],
 });
