937048c92bcee85a4c9d5ba7c189edcaa199162b
Trevor Blades <tdblades@gmail.com>
2026-06-08 12:02:45 -0700

    responsive styles

--- src/components/NewsletterForm.tsx
+++ src/components/NewsletterForm.tsx
@@ -14,10 +14,12 @@ export const NewsletterForm: React.FC = () => {
   if (status === "success") {
     return (
       <>
-        <p className="font-bold text-lg uppercase tracking-wide">
+        <p className="font-bold text-xs uppercase leading-7.5 tracking-wide md:text-lg md:leading-11">
           <Wave>Thanks for signing up! ✌️</Wave>
         </p>
-        <p>We won't bother you often, we hate that shit.</p>
+        <p className="tracking-wide max-md:text-xs">
+          We won't bother you often, we hate that shit.
+        </p>
       </>
     );
   }
@@ -25,7 +27,7 @@ export const NewsletterForm: React.FC = () => {
   return (
     <>
       <form
-        className="flex gap-4"
+        className="flex gap-3 md:gap-4"
         onSubmit={(event) => {
           event.preventDefault();
 
@@ -39,17 +41,19 @@ export const NewsletterForm: React.FC = () => {
           name="email"
           placeholder="Email"
           aria-invalid={isInputError(error) && error.fields.email !== undefined}
-          className="h-11 w-2xs rounded-xl border text-center font-bold tracking-wider focus-visible:outline focus-visible:outline-purple focus-visible:-outline-offset-1 aria-invalid:border-red-400"
+          className="h-7.5 w-51.5 rounded-lg border text-center font-bold tracking-wider focus-visible:outline focus-visible:outline-purple focus-visible:-outline-offset-1 aria-invalid:border-red-400 max-md:text-xs md:h-11 md:w-75.5 md:rounded-xl"
         />
         <button
           disabled={status === "pending"}
           type="submit"
-          className="flex size-11 not-disabled:cursor-pointer items-center justify-center rounded-xl border hover:border-none hover:bg-purple hover:text-black focus-visible:outline focus-visible:outline-purple focus-visible:-outline-offset-1"
+          className="flex size-7.5 not-disabled:cursor-pointer items-center justify-center rounded-lg border hover:border-none hover:bg-purple hover:text-black focus-visible:outline focus-visible:outline-purple focus-visible:-outline-offset-1 md:size-11 md:rounded-xl"
         >
-          <ArrowIcon className="size-5.75 fill-current" />
+          <ArrowIcon className="size-3.5 fill-current md:size-5.75" />
         </button>
       </form>
-      <p className="text-center tracking-wide">Join for updates & discounts.</p>
+      <p className="tracking-wide max-md:text-xs">
+        Join for updates & discounts.
+      </p>
     </>
   );
 };
